c3a6fdd48f35325c96ed21f1bc438264674e1436
Vivekpatel <vp20032020@gmail.com>
2025-01-13 01:47:39 +0530

    Find the Age Push the code

---
+++ Day-01/Level-01/Findage.java
@@ -0,0 +1,11 @@
+public class Findage{
+   public static void main( String[] args){
+	// created the  variables currentAge and birthYear
+    int currentYear = 2024;
+	int birthYear  = 2000;
+	//created variable currentAge and Calculated 
+	int currentAge = currentYear - birthYear;
+	System.out.println(" Harry's age in 2024 is " + currentAge ); 
+   }
+}
+
