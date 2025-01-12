ebc6be7fb364017791b00a716079960f199a37ec
Vivekpatel <vp20032020@gmail.com>
2025-01-13 01:49:04 +0530

    Average Percentage Code Push the code

---
+++ Day-01/Level-01/AveragePercentage.java
@@ -0,0 +1,12 @@
+public class AveragePercentage {
+    public static void main( String [] args ) {
+	// created the Variables mathsNumber, physicsNumber and chemistryNumber
+	   int mathsNumber = 94;
+	   int physicsNumber = 95;
+	   int chemistryNumber = 96;
+	   // Created the variable averagePercentage
+	   // calculate the averagePercentage
+	   double averagePercentage = (mathsNumber + physicsNumber + chemistryNumber)/3;
+	   System.out.println("Sam's average mark in PCM is " + averagePercentage);
+	}
+}
