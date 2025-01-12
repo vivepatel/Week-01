c743c539c2bf248e83fbff758cacf172a580b5da
Vivekpatel <vp20032020@gmail.com>
2025-01-13 00:11:01 +0530

    Unit converter  2 code Push the code

---
+++ Day-04/Level-02/UnitConverter_Two.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class UnitConverter_Two {
+
+    // Method to convert yards to feet
+    public static double convertYardsToFeet(double yards) {
+        double yards2feet = 3;
+        return yards * yards2feet;
+    }
+
+    // Method to convert feet to yards
+    public static double convertFeetToYards(double feet) {
+        double feet2yards = 0.333333;
+        return feet * feet2yards;
+    }
+
+    // Method to convert meters to inches
+    public static double convertMetersToInches(double meters) {
+        double meters2inches = 39.3701;
+        return meters * meters2inches;
+    }
+
+    // Method to convert inches to meters
+    public static double convertInchesToMeters(double inches) {
+        double inches2meters = 0.0254;
+        return inches * inches2meters;
+    }
+
+    // Method to convert inches to centimeters
+    public static double convertInchesToCentimeters(double inches) {
+        double inches2cm = 2.54;
+        return inches * inches2cm;
+    }
+
+    // Main method to demonstrate functionality
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+		System.out.println("Enter the Yards Feet Meters and Inches");
+        double yards = input.nextDouble();
+        double feet = input.nextDouble();
+        double meters = input.nextDouble();
+        double inches = input.nextDouble();
+
+        // Perform conversions and display results
+        System.out.println(yards + " yards = " + convertYardsToFeet(yards) + " feet");
+        System.out.println(feet + " feet = " + convertFeetToYards(feet) + " yards");
+        System.out.println(meters + " meters = " + convertMetersToInches(meters) + " inches");
+        System.out.println(inches + " inches = " + convertInchesToMeters(inches) + " meters");
+        System.out.println(inches + " inches = " + convertInchesToCentimeters(inches) + " centimeters");
+    }
+}
