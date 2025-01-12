8d3b188bf492a340375eff8ba8045ecdd8d2d891
Vivekpatel <vp20032020@gmail.com>
2025-01-13 01:50:47 +0530

    Conversion Kilometers to Miles Push the code

---
+++ Day-01/Level-01/CoversionKilometerstoMiles.java
@@ -0,0 +1,10 @@
+public class CoversionKilometerstoMiles{
+  public static void main ( String[] args ) {
+	  // created the  variables  kilometers and miles
+    double  Kilometers = 10.8;
+	// Here we Converted the  kilometers to miles
+	double  Miles = Kilometers * 0.6;
+	System.out.println(" The distance " + Kilometers + "km in miles  is  " + Miles );
+	
+  }
+}
